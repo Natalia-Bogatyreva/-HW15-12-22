0d965ede9bbb322b7dc5adbc8ea36c908e46356e
Natalia-Bogatyreva <rogova_nu@mail.ru>
2022-12-18 13:22:53 +0500

    Recurse M_N (sum)

---
+++ Task66/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// Задания выполняются только с помощью рекурсий!
+
+Console.WriteLine("Введите два числа, чтобы увидеть сумму натуральных элементов в промежутке между этими числами (включая их):");
+int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+
+if (m<n)
+{
+    NumSum(m,n,sum);
+}
+else
+{
+    NumSum(n,m,sum);
+}
+
+
+void NumSum(int m, int n, int sum)
+{
+    if (m>n)
+    {
+        Console.WriteLine($". Их сумма равна {sum}");
+        return;
+    }
+    sum = sum + m;
+    Console.Write(m +" ");
+    m++;
+    NumSum(m,n,sum);
+}
